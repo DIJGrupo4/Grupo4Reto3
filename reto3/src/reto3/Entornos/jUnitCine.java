f2c816e41944a7ef35d9a92736b5182c7ea22b2c
in1dw3 <in1dw3@HZ352023.elorrieta.int>
2023-02-03 20:08:27 +0100

    jUnit

--- reto3/src/reto3/Entornos/jUnitCine.java
+++ reto3/src/reto3/Entornos/jUnitCine.java
@@ -1,86 +1,63 @@
 package reto3.Entornos;
 
-import reto3.bbdd.pojo.Cine;
+import static org.junit.Assert.*;
 
 import java.io.Serializable;
 
 import org.junit.Test;
 
-public class jUnitCine {
+import reto3.bbdd.pojo.Cine;
 
-	
-	//***** para probar si el constructor funciona
-	@Test
-	public void testAccesoUsuarioConstructor() throws Exception {
-		AccesoUsuario accesoUsuario= AccesoUsuario.class.getConstructor().newInstance();
-		assertNotNull("El Usuario no puede ser creado !!!", accesoUsuario);
-	}
+class jUnitCine {
 
-	//***** para probar la serializaci�n	
-	@Test
-	public void testSerailizable() {
-		AccesoUsuario accesoUsuario= new AccesoUsuario();
-		assertTrue("No se puede realizar la serializaci�n!!!", accesoUsuario instanceof Serializable);
-	}
-	
-	//************* probar getter & Setters
-	
-	@Test
-	public void testId() {
-		AccesoUsuario accesoUsuario = new AccesoUsuario();
-		Long id=(long) 99999991;
-		accesoUsuario.setId(id);
-		assertNotEquals("Id no est� accesible", id, accesoUsuario.getId());
-	}
-	
-	@Test
-	public void testNombre() {
-		AccesoUsuario accesoUsuario = new AccesoUsuario();
-		String name= "un texto";
-		accesoUsuario.setNombre(name);
-		assertNotEquals("Nombre no est� accesible", name, accesoUsuario.getNombre());
-	}
-	
-	@Test
-	public void testUsuarioExiste() {
-		AccesoUsuario accesoUsuario= new AccesoUsuario();
-		accesoUsuario.setId((long) 1234);
-		accesoUsuario.setNombre("Zipi");
-		AccesoUsuario otroUsuario= new AccesoUsuario();
-		otroUsuario.setId((long) 1234);
-		otroUsuario.setNombre("Zipi");
+		//***** para probar la serializaci�n	
+		@Test
+		public void testSerailizable() {
+			Cine cine= new Cine();
+			assertTrue("No se puede realizar la serializacion!!!", cine instanceof Serializable);
+		}
 		
-		assertEquals("usuarios no son iguales!!!!", accesoUsuario, otroUsuario);	
+		//************* probar getter & Setters
 		
-	}
+		@Test
+		public void testcodCine() {
+			Cine cine= new Cine();
+			int codCine = 666;
+			cine.setCodCine(codCine);
+			assertNotEquals("codCine no esta accesible", codCine, cine.getCodCine());
+		}
+		
+		@Test
+		public void testNombre() {
+			Cine cine= new Cine();
+			String nombre= "un texto";
+			cine.setNombre(nombre);
+			assertNotEquals("Nombre no est� accesible", nombre, cine.getNombre());
+		}
+		
+		@Test
+		public void testDireccion() {
+			Cine cine= new Cine();
+			String direccion= "un texto";
+			cine.setNombre(direccion);
+			assertNotEquals("Nombre no est� accesible", direccion, cine.getNombre());
+		}
+		
+		@Test
+		public void testNumSalas() {
+			Cine cine= new Cine();
+			int numSalas= 500;
+			cine.setNumSalas(numSalas);
+			assertNotEquals("Nombre no est� accesible", numSalas, cine.getNombre());
+		}
+		
 
-	@Test
-	public void testUsuarioNoExiste() {
-		AccesoUsuario accesoUsuario= new AccesoUsuario();
-		accesoUsuario.setId((long) 1234);
-		accesoUsuario.setNombre("Zipi");
-		
-		AccesoUsuario otroUsuario= new AccesoUsuario();
-		otroUsuario.setId((long) 1234);
-		otroUsuario.setNombre("Zipi");
-		
-		assertNotEquals("usuarios son iguales!!!!", accesoUsuario, otroUsuario);	
-		
-		//*******
-		//otroUsuario.setId((long) 1234);
-		//otroUsuario.setNombre("Zipi");
-		
-		//assertNotEquals("usuarios son iguales!!!!", accesoUsuario, otroUsuario);	
-		
-	}
-
-	@Test
-	public void testHashCode() {
-	AccesoUsuario accesoUsuario=new AccesoUsuario();
-	assertEquals("hasCode no encontrado!!!",0, accesoUsuario.hashCode());
-	Long id=(long) 1234;
-	accesoUsuario.setId(id);
-	assertEquals("hashCode-2 no encontrado!!!!", id.hashCode(),accesoUsuario.hashCode());
-	}
-
+		@Test
+		public void testHashCode() {
+		Cine cine= new Cine();
+		assertEquals("hasCode no encontrado!!!",0, cine.hashCode());
+		int codCine = 666;
+		cine.setCodCine(codCine);
+		assertEquals("hashCode-2 no encontrado!!!!", hashCode(),cine.hashCode());
+		}
 }
