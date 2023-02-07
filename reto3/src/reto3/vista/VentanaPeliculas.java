3d1cbb689c515d54896c5089d7c31e57750e3a17
in1dw3 <in1dw3@HZ352023.elorrieta.int>
2023-02-07 16:58:27 +0100

    ventanas cambios

--- reto3/src/reto3/vista/VentanaPeliculas.java
+++ reto3/src/reto3/vista/VentanaPeliculas.java
@@ -3,14 +3,16 @@ package reto3.vista;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
 
 public class VentanaPeliculas {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -24,20 +26,30 @@ public class VentanaPeliculas {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public VentanaPeliculas() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
+
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 615, 488);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel seleccionPeliculasLogo = new JLabel("");
+		seleccionPeliculasLogo.setIcon(new ImageIcon("C:\\Users\\in1dw3\\Desktop\\Ibai\\Reto 3\\bienvenida.png"));
+		seleccionPeliculasLogo.setBounds(395, 25, 137, 172);
+		frame.getContentPane().add(seleccionPeliculasLogo);
+		
+		JPanel panelSelecPelis = new JPanel();
+		panelSelecPelis.setBounds(0, 252, 434, -251);
+		frame.getContentPane().add(panelSelecPelis);
+		
+		JLabel labelSeleccionPeliculas = new JLabel("Selecciona una Pelicula");
+		labelSeleccionPeliculas.setBounds(48, 92, 253, 29);
+		frame.getContentPane().add(labelSeleccionPeliculas);
+		labelSeleccionPeliculas.setHorizontalAlignment(SwingConstants.CENTER);
+		labelSeleccionPeliculas.setFont(new Font("Tahoma", Font.PLAIN, 24));
 	}
-
 }
