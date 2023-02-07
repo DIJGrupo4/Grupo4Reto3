593e593490754bea1923e161a78ae593e3ac5d0f
in1dw3 <in1dw3@HZ352023.elorrieta.int>
2023-02-07 15:33:11 +0100

    panel cines

--- reto3/src/reto3/vista/Reto3.java
+++ reto3/src/reto3/vista/Reto3.java
@@ -45,68 +45,67 @@ public class Reto3 {
 		frame.setBounds(100, 100, 580, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JPanel panelBienvenida = new JPanel();
 		panelBienvenida.setBounds(0, 0, 564, 441);
 		frame.getContentPane().add(panelBienvenida);
 		panelBienvenida.setLayout(null);
-		
+
 		JPanel panelSelecCines = new JPanel();
 		panelSelecCines.setBounds(0, 0, 564, 441);
 		frame.getContentPane().add(panelSelecCines);
 		panelSelecCines.setLayout(null);
-		
+
 		JButton botonBienvenida = new JButton("\r\n\r\n\r\n\r\nBIENVENIDO\r\n");
 		botonBienvenida.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				
+
 				panelBienvenida.setVisible(false);
 				panelSelecCines.setVisible(true);
-				
+
 			}
 
-
 		});
-		
-			botonBienvenida.setIcon(new ImageIcon("C:\\Users\\in1dw3\\Desktop\\Ibai\\Reto 3\\bienvenida.png"));
-			botonBienvenida.setBounds(0, 0, 564, 441);
-			botonBienvenida.setForeground(Color.BLACK);
-			botonBienvenida.setFont(new Font("Yu Gothic", Font.PLAIN, 50));
-			botonBienvenida.setBackground(new Color(255, 255, 240));
-			panelBienvenida.add(botonBienvenida);	
-		
+
+		botonBienvenida.setIcon(new ImageIcon("C:\\Users\\in1dw3\\Desktop\\Ibai\\Reto 3\\bienvenida.png"));
+		botonBienvenida.setBounds(0, 0, 564, 441);
+		botonBienvenida.setForeground(Color.BLACK);
+		botonBienvenida.setFont(new Font("Yu Gothic", Font.PLAIN, 50));
+		botonBienvenida.setBackground(new Color(255, 255, 240));
+		panelBienvenida.add(botonBienvenida);
+
 		JLabel labelSeleccionCines = new JLabel("Selecciona un Cine");
 		labelSeleccionCines.setHorizontalAlignment(SwingConstants.CENTER);
 		labelSeleccionCines.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		labelSeleccionCines.setBounds(33, 69, 241, 57);
 		panelSelecCines.add(labelSeleccionCines);
-		
-		
+
 		JLabel seleccionCinesLogo = new JLabel("");
 		seleccionCinesLogo.setBounds(376, 11, 137, 156);
 		panelSelecCines.add(seleccionCinesLogo);
-		
+
 		JButton seleccionBtnCine1 = new JButton("Cine1\r\n");
+		seleccionCinesLogo.setIcon(new ImageIcon("C:\\Users\\in1dw3\\Desktop\\Ibai\\Reto 3\\bienvenida.png"));
 		seleccionBtnCine1.setBounds(97, 184, 121, 74);
 		panelSelecCines.add(seleccionBtnCine1);
-		
+
 		JButton seleccionBtnCine2 = new JButton("Cine2\r\n");
 		seleccionBtnCine2.setBounds(317, 184, 127, 74);
 		panelSelecCines.add(seleccionBtnCine2);
-		
+
 		JButton seleccionBtnCine3 = new JButton("Cine3");
 		seleccionBtnCine3.setBounds(97, 282, 121, 74);
 		panelSelecCines.add(seleccionBtnCine3);
-		
+
 		JButton seleccionBtnCine4 = new JButton("Cine4\r\n");
 		seleccionBtnCine4.setBounds(317, 282, 127, 74);
 		panelSelecCines.add(seleccionBtnCine4);
-		
+
 		JButton btnFinalizar = new JButton("FINALIZAR");
 		btnFinalizar.setBounds(221, 392, 96, 38);
 		panelSelecCines.add(btnFinalizar);
-			
+
 	}
-	
+
 }
