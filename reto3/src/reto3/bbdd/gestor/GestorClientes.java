387f2461bd8862a53f68bd931dfde8108e48a14e
in1dw3 <in1dw3@HZ352012.elorrieta.int>
2023-02-10 19:40:05 +0100

    gestor clientes, obtener los clientes

--- reto3/src/reto3/bbdd/gestor/GestorClientes.java
+++ reto3/src/reto3/bbdd/gestor/GestorClientes.java
@@ -1,5 +1,91 @@
 package reto3.bbdd.gestor;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import reto3.bbdd.pojo.Cliente;
+import reto3.bbdd.utils.DBUtils;
+
 public class GestorClientes {
+
+	public ArrayList<Cliente> obtenerTodosLosClientes() {
+		ArrayList<Cliente> ret = null;
+		String sql = "select * from t_cliente";
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		// --------------------------------------------------------------------------------------------------
+		try {
+			// El Driver que vamos a usar
+			Class.forName(DBUtils.DRIVER);
+
+			// Abrimos la conexion con BBDD
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			// Vamos a lanzar la sentencia...
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+
+			// No es posible saber cuantas cosas nos ha devuelto el resultSet.
+			// Hay que ir 1 por 1 y guardandolo todo en su objeto Ejemplo correspondiente
+			while (resultSet.next()) {
+
+				// Si es necesario, inicializamos la lista
+				if (null == ret)
+					ret = new ArrayList<Cliente>();
+
+				Cliente cliente = new Cliente();
+
+				// Sacamos las columnas del RS
+				int codCliente = resultSet.getInt("codCliente");
+				String nombre = resultSet.getString("nombre");
+				String apellido = resultSet.getString("apellido");
+				String sexo = resultSet.getString("sexo");
+				String usuario = resultSet.getString("usuario");
+				String contraseña = resultSet.getString("contraseña");
+
+				// Metemos los datos a Ejemplo
+				cliente.setCodCliente(codCliente);
+				cliente.setNombre(nombre);
+				cliente.setApellido(apellido);
+				cliente.setSexo(sexo);
+				cliente.setUsuario(usuario);
+				cliente.setContraseña(contraseña);
+
+				// Lo guardamos en ret
+				ret.add(cliente);
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			// Cerramos al reves de como las abrimos
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+		}
+		return ret;
+	}
 
 }
