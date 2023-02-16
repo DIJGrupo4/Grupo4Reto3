679a7aa8845263a107effcfd0f99cc050f6de258
pedro <pedro@DESKTOP-G9DE6C5>
2023-02-16 23:06:46 +0100

    Adición codigo para comprobar login

--- reto3/src/reto3/bbdd/gestor/GestorClientes.java
+++ reto3/src/reto3/bbdd/gestor/GestorClientes.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 //import reto3.bbdd.menu.inicio;
 import reto3.bbdd.utils.DBUtils;
 import reto3.vista.paneles.CrearUsuario;
@@ -66,4 +68,82 @@ public class GestorClientes {
 
 		return ret;
 	}
+	public void validarLogin(){
+		
+		CrearUsuario usuLogin = new CrearUsuario();
+		
+		String usuario  = usuLogin.txtUsuario.getText();
+		String password = usuLogin.txtContraseña.getText();
+		
+		Connection connection = null;
+
+		// Vamos a lanzar una sentencia SQL contra la BBDD
+		Statement statement = null;
+		
+		// ResultSet
+		ResultSet resultSet = null;
+
+		try {
+			
+			//variables usuario correcto
+			String usuarioCorrecto = null;
+		    String passwordCorrecta = null;
+			
+			// El Driver que vamos a usar
+			Class.forName(DBUtils.DRIVER);
+
+			// Abrimos la conexion con BBDD
+			connection = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASS);
+
+			// Vamos a lanzar la sentencia...
+			statement = connection.createStatement();
+
+			// Montamos la SQL
+			String sql = "select usuario, contraseña from t_clientes";
+
+			// La ejecutamos...
+			statement.executeUpdate(sql);
+			
+			if(resultSet.next()) {
+				usuarioCorrecto = resultSet.getString(1);
+		        passwordCorrecta = resultSet.getString(2);
+			}
+			
+			if (usuarioCorrecto !=null && password!=null && password.equals(passwordCorrecta)) {
+				//usuLogin.panelLogin.setVisible(false);
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+			}
+
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			// Cerramos al reves de como las abrimos
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+		}
+	}
+		
 }
--- reto3/src/reto3/vista/paneles/CrearUsuario.java
+++ reto3/src/reto3/vista/paneles/CrearUsuario.java
@@ -27,6 +27,8 @@ public class CrearUsuario {
 	public JTextField txtUsuario = null;
 	public JTextField txtContraseña = null;
 	public JComboBox comboBoxSexo =null;
+	private JTextField txtUserLogin;
+	private JTextField txtUserPass;
 
 	/**
 	 * Launch the application.
@@ -62,102 +64,170 @@ public class CrearUsuario {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
+		//panel Login
+		JPanel panelLogin = new JPanel();
+		panelLogin.setBounds(0, 0, 434, 261);
+		frame.getContentPane().add(panelLogin);
+		panelLogin.setLayout(null);
+		
+		//panel registro
 		JPanel panelRegistro = new JPanel();
 		panelRegistro.setBounds(0, 0, 434, 261);
 		frame.getContentPane().add(panelRegistro);
 		panelRegistro.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Regristrarme");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel.setBounds(145, 11, 135, 26);
-		panelRegistro.add(lblNewLabel);
-
-		JLabel lblNewLabelNombre = new JLabel("Nombre");
-		lblNewLabelNombre.setBounds(10, 60, 46, 14);
-		panelRegistro.add(lblNewLabelNombre);
-
-		JLabel lblNewLabelApellido = new JLabel("Apellido");
-		lblNewLabelApellido.setBounds(10, 96, 46, 14);
-		panelRegistro.add(lblNewLabelApellido);
-
-		JLabel lblNewLabelDni = new JLabel("DNI");
-		lblNewLabelDni.setBounds(10, 132, 46, 14);
-		panelRegistro.add(lblNewLabelDni);
-
-		JLabel lblNewLabelSexo = new JLabel("Sexo");
-		lblNewLabelSexo.setBounds(10, 168, 46, 14);
-		panelRegistro.add(lblNewLabelSexo);
-
-		JLabel lblNewLabelUsuario = new JLabel("Usuario");
-		lblNewLabelUsuario.setBounds(224, 60, 46, 14);
-		panelRegistro.add(lblNewLabelUsuario);
-
-		JLabel lblNewLabelContraseña = new JLabel("Contraseña");
-		lblNewLabelContraseña.setBounds(224, 96, 56, 14);
-		panelRegistro.add(lblNewLabelContraseña);
-
-		txtNombre = new JTextField();
-		txtNombre.setBounds(74, 57, 86, 20);
-		panelRegistro.add(txtNombre);
-		txtNombre.setColumns(10);
-
-		txtApellido = new JTextField();
-		txtApellido.setBounds(74, 93, 86, 20);
-		panelRegistro.add(txtApellido);
-		txtApellido.setColumns(10);
-
-		txtDni = new JTextField();
-		txtDni.setBounds(74, 129, 86, 20);
-		panelRegistro.add(txtDni);
-		txtDni.setColumns(10);
 		
-		JComboBox comboBoxSexo = new JComboBox();
-		comboBoxSexo.setModel(new DefaultComboBoxModel(new String[] { "Seleccione", "Hombre", "Mujer" }));
-		comboBoxSexo.setBounds(74, 164, 86, 22);
-		panelRegistro.add(comboBoxSexo);
-
-		txtUsuario = new JTextField();
-		txtUsuario.setBounds(294, 57, 86, 20);
-		panelRegistro.add(txtUsuario);
-		txtUsuario.setColumns(10);
-
-		txtContraseña = new JTextField();
-		txtContraseña.setBounds(294, 93, 86, 20);
-		panelRegistro.add(txtContraseña);
-		txtContraseña.setColumns(10);
-
-		JButton btnNewButtonAtras = new JButton("Atrás");
-		btnNewButtonAtras.addActionListener(new ActionListener() {
+				
+		//Label login
+		JLabel lblLogin = new JLabel("LOGIN");
+		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblLogin.setBounds(188, 40, 66, 30);
+		panelLogin.add(lblLogin);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		lblNewLabel_1.setBounds(198, 81, 46, 14);
+		panelLogin.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Contraseña");
+		lblNewLabel_2.setBounds(188, 137, 72, 14);
+		panelLogin.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("¿No tienes cuenta? Registrate gratis");
+		lblNewLabel_3.setBounds(126, 15, 184, 14);
+		panelLogin.add(lblNewLabel_3);
+		
+		//TextField login
+		txtUserLogin = new JTextField();
+		txtUserLogin.setBounds(178, 106, 86, 20);
+		panelLogin.add(txtUserLogin);
+		txtUserLogin.setColumns(10);
+		
+		txtUserPass = new JTextField();
+		txtUserPass.setBounds(181, 163, 86, 20);
+		panelLogin.add(txtUserPass);
+		txtUserPass.setColumns(10);
+		
+		//Botones login
+		JButton btnAccederCuenta = new JButton("Acceder");
+		btnAccederCuenta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panelRegistro.setVisible(false);
+				GestorClientes gestCli = new GestorClientes();
+				gestCli.validarLogin();
+				
 			}
 		});
-		btnNewButtonAtras.setBounds(222, 227, 89, 23);
-		panelRegistro.add(btnNewButtonAtras);
-
-		JButton btnNewButtonAceptar = new JButton("Aceptar");
-		btnNewButtonAceptar.addActionListener(new ActionListener() {
+		btnAccederCuenta.setBounds(178, 227, 89, 23);
+		panelLogin.add(btnAccederCuenta);
+		
+		JButton btnNewButton = new JButton("Registrarme");
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
-				Cliente clienteNuevo = new Cliente();
-
-				clienteNuevo.setNombre(txtNombre.getText());
-				clienteNuevo.setApellido(txtApellido.getText());
-				clienteNuevo.setDNI(txtDni.getText());
-				clienteNuevo.setSexo(comboBoxSexo.getSelectedItem().toString());
-				clienteNuevo.setUsuario(txtUsuario.getText());
-				clienteNuevo.setContraseña(txtContraseña.getText());
-
-				gestorCliente.añadirUsuario(clienteNuevo);
-
-				JFrame jFrame = new JFrame();
-				JOptionPane.showMessageDialog(jFrame, "Cuenta creada");
-				panelRegistro.setVisible(false);
-				// Ventana login()
+				panelRegistro.setVisible(true);
+				panelLogin.setVisible(false);
 			}
 		});
-		btnNewButtonAceptar.setBounds(321, 227, 89, 23);
-		panelRegistro.add(btnNewButtonAceptar);
+		btnNewButton.setBounds(320, 11, 104, 23);
+		panelLogin.add(btnNewButton);
+		
+		
+		//labels registro
+				JLabel lblNewLabel = new JLabel("Regristrarme");
+				lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+				lblNewLabel.setBounds(145, 11, 135, 26);
+				panelRegistro.add(lblNewLabel);
+
+				JLabel lblNewLabelNombre = new JLabel("Nombre");
+				lblNewLabelNombre.setBounds(10, 60, 46, 14);
+				panelRegistro.add(lblNewLabelNombre);
+
+				JLabel lblNewLabelApellido = new JLabel("Apellido");
+				lblNewLabelApellido.setBounds(10, 96, 46, 14);
+				panelRegistro.add(lblNewLabelApellido);
+
+				JLabel lblNewLabelDni = new JLabel("DNI");
+				lblNewLabelDni.setBounds(10, 132, 46, 14);
+				panelRegistro.add(lblNewLabelDni);
+
+				JLabel lblNewLabelSexo = new JLabel("Sexo");
+				lblNewLabelSexo.setBounds(10, 168, 46, 14);
+				panelRegistro.add(lblNewLabelSexo);
+
+				JLabel lblNewLabelUsuario = new JLabel("Usuario");
+				lblNewLabelUsuario.setBounds(224, 60, 46, 14);
+				panelRegistro.add(lblNewLabelUsuario);
+
+				JLabel lblNewLabelContraseña = new JLabel("Contraseña");
+				lblNewLabelContraseña.setBounds(224, 96, 56, 14);
+				panelRegistro.add(lblNewLabelContraseña);
+
+				
+				//textField registro
+				txtNombre = new JTextField();
+				txtNombre.setBounds(74, 57, 86, 20);
+				panelRegistro.add(txtNombre);
+				txtNombre.setColumns(10);
+
+				txtApellido = new JTextField();
+				txtApellido.setBounds(74, 93, 86, 20);
+				panelRegistro.add(txtApellido);
+				txtApellido.setColumns(10);
+
+				txtDni = new JTextField();
+				txtDni.setBounds(74, 129, 86, 20);
+				panelRegistro.add(txtDni);
+				txtDni.setColumns(10);
+
+				txtUsuario = new JTextField();
+				txtUsuario.setBounds(294, 57, 86, 20);
+				panelRegistro.add(txtUsuario);
+				txtUsuario.setColumns(10);
+
+				txtContraseña = new JTextField();
+				txtContraseña.setBounds(294, 93, 86, 20);
+				panelRegistro.add(txtContraseña);
+				txtContraseña.setColumns(10);
+				
+				//Combo box registro
+				JComboBox comboBoxSexo = new JComboBox();
+				comboBoxSexo.setModel(new DefaultComboBoxModel(new String[] { "Seleccione", "Hombre", "Mujer" }));
+				comboBoxSexo.setBounds(74, 164, 86, 22);
+				panelRegistro.add(comboBoxSexo);
+
+				//botones registro
+				JButton btnNewButtonAtras = new JButton("Atrás");
+				btnNewButtonAtras.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						panelRegistro.setVisible(false);
+						panelLogin.setVisible(true);
+					}
+				});
+				btnNewButtonAtras.setBounds(222, 227, 89, 23);
+				panelRegistro.add(btnNewButtonAtras);
+
+				JButton btnNewButtonAceptar = new JButton("Aceptar");
+				btnNewButtonAceptar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+
+						Cliente clienteNuevo = new Cliente();
+
+						clienteNuevo.setNombre(txtNombre.getText());
+						clienteNuevo.setApellido(txtApellido.getText());
+						clienteNuevo.setDNI(txtDni.getText());
+						clienteNuevo.setSexo(comboBoxSexo.getSelectedItem().toString());
+						clienteNuevo.setUsuario(txtUsuario.getText());
+						clienteNuevo.setContraseña(txtContraseña.getText());
+
+						gestorCliente.añadirUsuario(clienteNuevo);
+
+						JFrame jFrame = new JFrame();
+						JOptionPane.showMessageDialog(jFrame, "Cuenta creada");
+						panelRegistro.setVisible(false);
+						panelLogin.setVisible(true);
+					}
+				});
+				btnNewButtonAceptar.setBounds(321, 227, 89, 23);
+				panelRegistro.add(btnNewButtonAceptar);
+
 
 		
 	}
