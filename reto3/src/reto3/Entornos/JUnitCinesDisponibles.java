4a4df1842fd7b1352b847765503ac4e8e067cd0a
Ibai <Ibai@DESKTOP-80GD2CM>
2023-02-24 11:28:00 +0100

    junit cinedisponibles hecho

--- reto3/src/reto3/Entornos/JUnitCinesDisponibles.java
+++ reto3/src/reto3/Entornos/JUnitCinesDisponibles.java
@@ -1,108 +1,51 @@
 package reto3.Entornos;
 
-import static org.junit.Assert.*; 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
+import reto3.bbdd.gestor.GestorCines;
 import reto3.bbdd.pojo.Cine;
 
 class JUnitCinesDisponibles {
 
-	
-
 	@Test
-	void testCineBilbaoTrue() {
+	void testCineDisponiblesNotNull() {
 
-		Cine cine = new Cine();
-		String cineBilbao = cine.getNombre();
-		assertTrue("Cine disponible:'" + cineBilbao + "'", true);
+		GestorCines gestorCines = new GestorCines();
+		ArrayList<Cine> cine = null;
+		cine = gestorCines.obtenerTodosLosCines();
+		assertNotNull(cine);
 	}
 
 	@Test
-	void testCineBilbaoFalse() {
+	void testCineDisponiblesValue() {
 
-		Cine cine = new Cine();
-		String cineBilbao = cine.getNombre();
-		assertFalse("Cine no disponible:'" + cineBilbao + "'", false);
-	}
-	
-	@Test
-	void testCineBilbaoNull() {
+		GestorCines gestorCines = new GestorCines();
+		ArrayList<Cine> cine = null;
+		cine = gestorCines.obtenerTodosLosCines();
+		String nombreCine = null;
+		String nombreEsperado = "CineBilbao";
 
-		Cine cine = new Cine();
-		String cineBilbao = cine.getNombre();
-		assertNull("Cine no disponible:'" + cineBilbao + "'", null);
+		nombreCine = cine.get(0).getNombre();
+
+		assertEquals(nombreCine, nombreEsperado);
 	}
 
 	@Test
-	void testCineBasauriTrue() {
+	void testCineDisponiblesTamaño() {
 
-		Cine cine = new Cine();
-		String cineBasauri = cine.getNombre();
-		assertTrue("Cine disponible:'" + cineBasauri + "'", true);
-	}
+		GestorCines gestorCines = new GestorCines();
+		ArrayList<Cine> cine = null;
+		cine = gestorCines.obtenerTodosLosCines();
+		int tamañoCines = 0;
+		int tamañoEsperado = 4;
 
-	@Test
-	void testCineBasauriFalse() {
+		tamañoCines = cine.get(0).getCodCine();
 
-		Cine cine = new Cine();
-		String cineBasauri = cine.getNombre();
-		assertFalse("Cine no disponible:'" + cineBasauri + "'", false);
-	}
-	
-	@Test
-	void testCineBasauriNull() {
-
-		Cine cine = new Cine();
-		String cineBasauri = cine.getNombre();
-		assertNull("Cine no disponible:'" + cineBasauri + "'", null);
-	}
-
-	@Test
-	void testCineBaracaldoTrue() {
-
-		Cine cine = new Cine();
-		String cineBaracaldo = cine.getNombre();
-		assertTrue("Cine disponible:'" + cineBaracaldo + "'", true);
-	}
-
-	@Test
-	void testCineBaracaldoFalse() {
-
-		Cine cine = new Cine();
-		String cineBaracaldo = cine.getNombre();
-		assertFalse("Cine no disponible:'" + cineBaracaldo + "'", false);
-	}
-	
-	@Test
-	void testCineBaracaldoNull() {
-
-		Cine cine = new Cine();
-		String cineBaracaldo = cine.getNombre();
-		assertNull("Cine no disponible:'" + cineBaracaldo + "'", null);
-	}
-
-	@Test
-	void testCineElorrietaTrue() {
-
-		Cine cine = new Cine();
-		String cineElorrieta = cine.getNombre();
-		assertTrue("Cine disponible:'" + cineElorrieta + "'", true);
-	}
-
-	@Test
-	void testCineElorrietaFalse() {
-
-		Cine cine = new Cine();
-		String cineElorrieta = cine.getNombre();
-		assertFalse("Cine no disponible:'" + cineElorrieta + "'", false);
-	}
-	
-	@Test
-	void testCineElorrietaNull() {
-
-		Cine cine = new Cine();
-		String cineElorrieta = cine.getNombre();
-		assertNull("Cine no disponible:'" + cineElorrieta + "'", null);
+		assertEquals(tamañoCines, tamañoEsperado);
 	}
 
 }
